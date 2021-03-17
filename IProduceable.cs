ae2851f9edfeae1e08a42bf0d9e33c65e3de3146
Anna Onofriichuk <41467875+anna-onofriichuk11@users.noreply.github.com>
2021-03-17 14:21:48 +0200

    Update IProduceable.cs

--- IProduceable.cs
+++ IProduceable.cs
@@ -1,16 +1,14 @@
-﻿using COMP442_Assignment2.Tokens;
+﻿using Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace COMP442_Assignment2.Syntactic
+namespace Syntactic
 {
     /*
         A symbol in the grammar used by the syntactic anaylzer
-
-        For COMP 442 Assignment 2 by Michael Bilinsky 26992358
     */
     public interface IProduceable
     {
